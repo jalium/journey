2a00c12685f6b9e3afd720417564bde949b28cc3
Jonathan Nguyen <jonathannguyen@jonathans-air.wireless.concordia.ca>
2019-08-13 16:39:46 -0400

    Added listing card class , tentatively finished Experiences class

--- alibay-boiler/src/Experiences.jsx
+++ alibay-boiler/src/Experiences.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import SellerForm from "./SellerForm.jsx";
+import ListingCard from "./ListingCard.jsx";
+
 class Experience extends Component {
   constructor(props) {
     super(props);
@@ -9,20 +10,35 @@ class Experience extends Component {
     };
   }
 
-  reload = async () => {
-    let response = await fetch("/experiences");
-    let body = await response.text();
-    console.log("/experiences response", body);
-    body = JSON.parse(body);
-    this.setState({ posts: body });
+  componentDidMount = () => {
+    reload = async () => {
+      let response = await fetch("/experiences");
+      let body = await response.text();
+      console.log("/experiences response", body);
+      body = JSON.parse(body);
+      this.setState({ posts: body });
+    };
   };
+
   render = () => {
     return (
       <div>
-        {this.state.posts.map(p => {
-          p;
+        {this.state.posts.map(post => {
+          return (
+            <ListingCard
+              listingTitle={post.listingTitle}
+              destination={post.destination}
+              amenities={post.amenities}
+              rating={post.rating}
+              date={post.date}
+              price={post.price}
+              frontendPath={post.frontendPath}
+            />
+          );
         })}
       </div>
     );
   };
 }
+
+export default Experiences;
---
+++ alibay-boiler/src/ListingCard.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import "./main.css";
+
+class ListingCard extends Component  {
+render = () => {
+    return (<div className="listing card">
+
+    </div>)
+}
+}
+
+export default ListingCard
