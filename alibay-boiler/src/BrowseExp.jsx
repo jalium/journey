e9f84b7af9a41d90f3639d34d3a15a9a9c2c86a5
jalium <jaliumj@gmail.com>
2019-08-21 19:11:15 -0400

    Browse card css

--- alibay-boiler/src/BrowseExp.jsx
+++ alibay-boiler/src/BrowseExp.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./main.css";
-import Checkout from "./Checkout.jsx"
+import Checkout from "./Checkout.jsx";
 
 class BrowseExp extends Component {
   render = () => {
     return (
       <div className="card">
-        <Link to={"/experiences/" + this.props.listingId}>
-          {this.props.listingTitle}
-        </Link>
-        <div>destination: {this.props.destination}</div>
-        <div>rating: {this.props.rating}</div>
-        <div>total price: {this.props.price}</div>
+        <div>
+          <Link to={"/experiences/" + this.props.listingId}>
+            {this.props.listingTitle}
+          </Link>
+          <br />
+          <br />
+          <div>destination: {this.props.destination}</div>
+          <div> {this.props.rating}</div>
+          <div>total price: {"$" + this.props.price}</div>
+        </div>
         <img height="100px" src={this.props.frontendPath} />
       </div>
     );
